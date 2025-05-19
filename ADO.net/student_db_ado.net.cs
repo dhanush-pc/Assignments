7f04cee92b7f7f5a2c846d55553fe582e57321da
dhanush-pc <140696494+dhanush-pc@users.noreply.github.com>
2025-05-19 11:06:05 +0530

    Update student_db_ado.net.cs

--- ADO.net/student_db_ado.net.cs
+++ ADO.net/student_db_ado.net.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 namespace pract2
 {
-        
-        internal class Program
+
+    class Program
     {
         private static string connectionString = "Server=LTIN563416\\SQLEXPRESS;Database=MySqlServerDatabase;Integrated Security=True;";
 
@@ -21,8 +21,8 @@ namespace pract2
                     string sql = @"
                     IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Student')
                     CREATE TABLE Student (
-                        Id INT PRIMARY KEY IDENTITY(1,1),
-                        Name NVARCHAR(255) NOT NULL,
+                        Id INT PRIMARY KEY,
+                        Name VARCHAR(255) NOT NULL,
                         Age INT
                     );";
                     using (SqlCommand command = new SqlCommand(sql, connection))
@@ -38,16 +38,17 @@ namespace pract2
             }
         }
 
-        public static void InsertStudent(string name, int age)
+        public static void InsertStudent(string name, int age,int id)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 try
                 {
                     connection.Open();
-                    string sql = "INSERT INTO Student (Name, Age) VALUES (@Name, @Age);";
+                    string sql = "INSERT INTO Student (Id, Name, Age) VALUES (@Id, @Name, @Age);";
                     using (SqlCommand command = new SqlCommand(sql, connection))
                     {
+                        command.Parameters.AddWithValue("@Id", id);
                         command.Parameters.AddWithValue("@Name", name);
                         command.Parameters.AddWithValue("@Age", age);
                         int rowsAffected = command.ExecuteNonQuery();
@@ -174,22 +175,83 @@ namespace pract2
 
         static void Main(string[] args)
         {
-            CreateStudentTable();
+            Console.WriteLine("********* WELCOME TO STUDENT MANAGEMENT SYSTEM ********");
+            Console.WriteLine("What do you want to do:");
+            Console.WriteLine("1. Create student\n" +
+                              "2. insert student\n" +
+                              "3. get student by id \n" +
+                              "4. get all students\n" +
+                              "5. update\n" +
+                              "6. delete\n" +
+                              "7.clear\n" +
+                              "8. Exit");
 
-            InsertStudent("Alice", 20);
-            InsertStudent("Bob", 22);
-            InsertStudent("Charlie", 21);
+            string stay = "Y";
 
-            GetStudentById(1);
-            GetAllStudents();
+            while (stay == "Y")
+            {
+                Console.WriteLine("Enter your choice:");
+                int choice = Convert.ToInt32(Console.ReadLine());
 
-            UpdateStudent(2, "Robert", 23);
-            GetStudentById(2);
+                switch (choice)
+                {
+                    case 1:
+                        Program.CreateStudentTable();
+                        break;
+                    case 2:
+                        String name;
+                        int age;
+                        Console.WriteLine("enter ID");
+                        int id0 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter student name:");
+                        name = Console.ReadLine();
+                        Console.WriteLine("Enter student age:");
+                        age = Convert.ToInt32(Console.ReadLine());
+                        Program.InsertStudent(name,age,id0);
+                        break;
+                    case 3:
+                        int id;
+                        Console.WriteLine("Enter student ID u want to retrive:");
+                        id = Convert.ToInt32(Console.ReadLine());
+                        Program.GetStudentById(id);
+                        break;
+                    case 4:
+                        Program.GetAllStudents();
+                        break;
+                    case 5:
+                        int id1;
+                        string name1;
+                        int age1;
+                        Console.WriteLine("Enter student ID to update:");
+                        id1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter new student name:");
+                        name1 = Console.ReadLine();
+                        Console.WriteLine("Enter new student age (or leave blank to keep current age):");
+                        age1 = Convert.ToInt32(Console.ReadLine());
+                        Program.UpdateStudent(id1,name1,age1);
+                        break;
+                    case 6:
+                        Console.WriteLine("Enter student ID to delete:");
+                        int id2 = Convert.ToInt32(Console.ReadLine());
+                        Program.DeleteStudent(id2);
+                        break;
+                    case 7:
+                        Console.Clear();
+                        break;
+                    case 8:
+                        Console.WriteLine("Exiting...");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice!");
+                        break;
+                }
 
-            DeleteStudent(3);
-            GetAllStudents();
-
-
+                Console.WriteLine("Choose another option or exit? (Y/N)");
+                stay = Console.ReadLine().ToUpper();
+            }
         }
+
+
     }
 }
